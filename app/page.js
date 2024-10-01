fd4b64cef3d4dcd226ae7e77461fcdb2315f9f1c
TTMac135 <tyler.mac@edu.sait.ca>
2024-10-01 15:42:53 -0600

    Finish Week 3 demo

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Page() {
         <Link href="week-2" className={linkStyles}>Week 2</Link>
       </li>
       <li>
-        <Link href="week-3" className={linkStyles}>Week 3 (Wasn't in class)</Link>
+        <Link href="week-3" className={linkStyles}>Week 3</Link>
       </li>
       <li>
         <Link href="./week-4/functions" className={linkStyles} >Week 4 - Functions</Link> <Link href="./week-4/simple-counter" className={linkStyles} >Simple Counter</Link> <Link href="./week-4/advanced-counter" className={linkStyles} >Advanced Counter</Link>
